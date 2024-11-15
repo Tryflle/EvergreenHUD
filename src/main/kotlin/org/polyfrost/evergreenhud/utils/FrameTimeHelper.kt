cec41856ef92e0c2857703018dbdf31ecb08d8e7
nextdayy <79922345+nextdayy@users.noreply.github.com>
2024-11-15 19:54:23 +0000

    oop!

--- src/main/kotlin/org/polyfrost/evergreenhud/utils/FrameTimeHelper.kt
+++ src/main/kotlin/org/polyfrost/evergreenhud/utils/FrameTimeHelper.kt
@@ -5,13 +5,13 @@ import org.polyfrost.oneconfig.api.event.v1.events.RenderEvent
 import org.polyfrost.oneconfig.api.event.v1.events.TickEvent
 
 object FrameTimeHelper {
-    private var lastTime = System.currentTimeMillis().toDouble()
-    val frameTimes = ArrayList<Double>(60)
+    private var lastTime = System.currentTimeMillis().toFloat()
+    val frameTimes = ArrayList<Float>(40)
 
     init {
         eventHandler { _: RenderEvent.End ->
             frameTimes += System.currentTimeMillis() - lastTime
-            lastTime = System.currentTimeMillis().toDouble()
+            lastTime = System.currentTimeMillis().toFloat()
         }
         eventHandler { _: TickEvent.End ->
             frameTimes.clear()
