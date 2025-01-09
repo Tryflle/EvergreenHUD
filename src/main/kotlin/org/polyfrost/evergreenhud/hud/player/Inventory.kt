1b804d946a9c32ee5ab345dcac31de326f52c251
nextdayy <79922345+nextdayy@users.noreply.github.com>
2025-01-09 15:59:52 +0000

    fix inventory hud and test things

--- src/main/kotlin/org/polyfrost/evergreenhud/hud/player/Inventory.kt
+++ src/main/kotlin/org/polyfrost/evergreenhud/hud/player/Inventory.kt
@@ -11,10 +11,10 @@ import net.minecraft.client.renderer.GlStateManager as GL
 
 class Inventory : LegacyHud() {
     override var width: Float
-        get() = 400f
+        get() = 176f
         set(_) {}
 
-    override var height = 200f
+    override var height = 93f
 
     @RadioButton(title = "Inventory", options = ["Player", "Ender Chest"])
     var type = 0
@@ -30,31 +30,32 @@ class Inventory : LegacyHud() {
 
     @Suppress("SENSELESS_COMPARISON", "NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
     override fun render(stack: UMatrixStack, x: Float, y: Float, scaleX: Float, scaleY: Float) {
+        val inv = inventory ?: return
         val mc = Minecraft.getMinecraft()
         val fr = mc.fontRendererObj
         val ri = mc.renderItem
         val gui = mc.ingameGUI
+
+        GL.pushMatrix()
         mc.textureManager.bindTexture(inventoryBackground)
-        GL.color(1.0f, 1.0f, 1.0f, 1.0f)
+        GL.color(1f, 1f, 1f, 1f)
         GL.enableRescaleNormal()
-        // todo positioning and scaling
+        GL.enableBlend()
+        GL.scale(scaleX, scaleY, 1f)
+        GL.translate(x * 1f / scaleX, y * 1f / scaleY, 0f)
         gui.drawTexturedModalRect(0, 0, 0, 0, 176, 3 * 18 + 17)
         gui.drawTexturedModalRect(0, 3 * 18 + 17, 0, 215, 176, 22)
-        fr.drawString("Inventory", 8, 6, 0)
+        fr.drawString(if (type == 0) "Inventory" else "Ender Chest", 8, 6, 0)
 
         RenderHelper.enableGUIStandardItemLighting()
-
-        val inv = inventory ?: return
-        val xi = 0
-        val yi = 0
         val base = if (type == 0) 9 else 0
         for (i in base until base + 27) {
             val row = i / 9
             val column = i % 9
             val item = inv.getStackInSlot(i)
             if (item != null) {
-                val itemX = xi + 8 + column * 18
-                val itemY = yi + row * 18
+                val itemX = 8 + column * 18
+                val itemY = row * 18
                 ri.renderItemAndEffectIntoGUI(item, itemX, itemY)
                 ri.renderItemOverlayIntoGUI(fr, item, itemX, itemY, null)
             }
@@ -62,6 +63,7 @@ class Inventory : LegacyHud() {
         RenderHelper.disableStandardItemLighting()
         GL.disableBlend()
         GL.disableRescaleNormal()
+        GL.popMatrix()
     }
 
     override fun category() = Category.PLAYER
@@ -69,4 +71,6 @@ class Inventory : LegacyHud() {
     override fun title() = "Inventory"
 
     override fun update() = false
+
+    override fun hasBackground() = false
 }
