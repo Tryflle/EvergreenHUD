637bb4d9e9894c6c3cd5e00990c3313b9d33fe5f
Empa <itsempa@users.noreply.github.com>
2026-02-19 09:27:57 +0100

    fix: saturation event getting sent on the server v2

--- src/main/java/org/polyfrost/evergreenhud/mixins/client/Mixin_FoodStats_SaturationChangedEvent.java
+++ src/main/java/org/polyfrost/evergreenhud/mixins/client/Mixin_FoodStats_SaturationChangedEvent.java
@@ -22,9 +22,4 @@ public class Mixin_FoodStats_SaturationChangedEvent {
     private void onReadNBT(CallbackInfo ci) {
         EventManager.INSTANCE.post(new SaturationChangedEvent(this.saturationLevel));
     }
-
-    @Inject(method = "setSaturation", at = @At("RETURN"))
-    private void onSet(CallbackInfo ci) {
-        EventManager.INSTANCE.post(new SaturationChangedEvent(this.saturationLevel));
-    }
 }
--- src/main/kotlin/org/polyfrost/evergreenhud/client/hud/SaturationHud.kt
+++ src/main/kotlin/org/polyfrost/evergreenhud/client/hud/SaturationHud.kt
@@ -1,19 +1,29 @@
 package org.polyfrost.evergreenhud.client.hud
 
+import net.minecraft.network.protocol.game.ClientboundSetHealthPacket
 import org.polyfrost.evergreenhud.client.SaturationChangedEvent
 import org.polyfrost.evergreenhud.client.utils.GenericNumberHud
 import org.polyfrost.oneconfig.api.event.v1.eventHandler
+import org.polyfrost.oneconfig.api.event.v1.events.PacketEvent
 
 // CHECK OK
 class SaturationHud : GenericNumberHud(
     title ="Saturation",
     category = Category.INFO
 ) {
+    private fun update(saturation: Float) {
+        value = saturation
+        updateAndRecalculate()
+    }
+
     override fun setup() {
         super.setup()
         eventHandler { (saturation): SaturationChangedEvent ->
-            value = saturation
-            updateAndRecalculate()
+            update(saturation)
+        }
+        eventHandler { (packet): PacketEvent.Receive ->
+            val packet = packet as? SaturationChangedEvent ?: return@eventHandler
+            update(packet.saturation)
         }
     }
 }
