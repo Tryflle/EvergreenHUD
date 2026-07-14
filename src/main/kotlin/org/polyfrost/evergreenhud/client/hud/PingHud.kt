fb8ad252b712bf03b9af48647020527358d956cd
awruff <sillysys@proton.me>
2026-07-14 10:29:32 -0400

    fixes #64

--- src/main/kotlin/org/polyfrost/evergreenhud/client/hud/PingHud.kt
+++ src/main/kotlin/org/polyfrost/evergreenhud/client/hud/PingHud.kt
@@ -1,10 +1,6 @@
 package org.polyfrost.evergreenhud.client.hud
 
 import org.polyfrost.evergreenhud.client.utils.CachedTextHud
-import org.polyfrost.evergreenhud.client.utils.pinger.ServerPinger
-import org.polyfrost.evergreenhud.client.utils.pinger.ServerPingerPool
-import org.polyfrost.oneconfig.api.config.v1.annotations.Slider
-import org.polyfrost.oneconfig.api.config.v1.annotations.Switch
 import org.polyfrost.oneconfig.api.event.v1.eventHandler
 import org.polyfrost.oneconfig.api.event.v1.events.WorldEvent
 import org.polyfrost.oneconfig.utils.v1.dsl.mc
@@ -14,22 +10,10 @@ class PingHud : CachedTextHud(
     title = "Ping",
     category = Category.INFO,
 ) {
-    @Slider(title = "Ping Period", min = 20F, max = 120F)
-    var interval = 20
-
-    private lateinit var pinger: ServerPinger
-
     override fun setup() {
         super.setup()
         eventHandler { _: WorldEvent.Load ->
             hidden = mc.hasSingleplayerServer()
-        }
-
-        if (isReal) {
-            pinger = ServerPingerPool.createPinger(
-                intervalSupplier = { interval },
-                serverSupplier = { mc.currentServer }
-            )
         }
     }
 
@@ -38,8 +22,9 @@ class PingHud : CachedTextHud(
     }
 
     override fun getText(): String {
-        return (if (isReal && ::pinger.isInitialized) {
-            pinger.ping?.toString() ?: "-1 (unknown)"
-        } else "-1") + "ms"
+        val ping = if (isReal) {
+            mc.player?.uuid?.let { mc.connection?.getPlayerInfo(it)?.latency }
+        } else null
+        return "${ping ?: -1}ms"
     }
 }
