6fee18d3c086f6e9946fedb54e9e0d40086a1aca
awruff <sillysys@proton.me>
2026-07-17 13:20:44 -0400

    chore: change entity counter's prefix
    fixes #105

--- src/main/kotlin/org/polyfrost/evergreenhud/client/hud/EntityCounterHud.kt
+++ src/main/kotlin/org/polyfrost/evergreenhud/client/hud/EntityCounterHud.kt
@@ -4,12 +4,11 @@ import org.polyfrost.evergreenhud.client.EntityCounterEvent
 import org.polyfrost.evergreenhud.client.utils.CachedTextHud
 import org.polyfrost.oneconfig.api.config.v1.annotations.Switch
 import org.polyfrost.oneconfig.api.event.v1.eventHandler
-import org.polyfrost.oneconfig.api.hud.v1.TextHud
 
 class EntityCounterHud : CachedTextHud(
     title = "Entity Counter",
     category = Category.INFO,
-    prefix = "E:",
+    prefix = "Entities:",
     defaultText = "0"
 ) {
     @Switch(title = "Show total entities")
